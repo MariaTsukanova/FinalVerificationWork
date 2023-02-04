186ee065e1bf41f21c482f33d137bb4fa8a86fc7
Maria <o.esipova1975@yandex.ru>
2023-02-04 14:38:31 +0300

    added Task

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿//Заданный массив
+string[] inPutArray = new string[4] {"hello", "234", "world", "cat"};
+// Метод создания массива из 3-х символьных строк
+string[] CreateArr(string[] array, int size)
+{
+    int j = 0; 
+   string[] outArr = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+   
+        if (array[i].Length < size+1)
+        {
+            outArr[j] = array[i];
+            j++;
+       }
+      
+
+    return outArr;
+}
+// Метод вывода массива
+void PrintArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+   Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] outPutArray = CreateArr(inPutArray,3);
+PrintArray(outPutArray);
